4bc43a1cef3bd4d5af3970bb7120e306a41ccf17
zwcwl <zc15722@163.com>
2022-07-11 18:17:25 +0800

    company-22-7-11

--- routes/menu.js
+++ routes/menu.js
@@ -5,23 +5,43 @@ const Menu = require("../models/menus")
 router.prefix("/menu")
 
 router.get("/", async ctx => {
+	let { menuName, menuState } = ctx.request.query
+	let params = {}
+	if (menuName) params.menuName = menuName
+	if (menuState) params.menuState = menuState
 
+	let res = await Menu.find(params)
+	recursion(res)
+	ctx.body = util.succeed(res)
 })
+let arr=[]
+function recursion(res){
+	res.forEach(element => {
+		if(element.parentId[0]==null){
+			arr.push(element)
+			console.log(arr);
+		}
+	});
+}
 
 router.post("/", async ctx => {
-	let {params} = ctx.request.body
+	let params = ctx.request.body
 	Menu.create(params)
 	ctx.body = util.succeed()
 })
 
 router.put("/", async ctx => {
-	let { _id, ...params } = ctx.request.body
-	console.log(params);
+	let params = ctx.request.body
+	await Menu.findByIdAndUpdate(params._id, params)
 	ctx.body = util.succeed()
 })
 
 router.delete("/", async ctx => {
-
+	console.log(ctx.request.body);
+	let id = ctx.request.body
+	await Menu.deleteMany({ parentId: { $all: [id] } })
+	await Menu.findByIdAndDelete(id)
+	ctx.body = util.succeed()
 })
 
 module.exports = router
